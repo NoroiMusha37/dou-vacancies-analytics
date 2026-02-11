4e005ae789f2c81944c90cb217b452abb6cc5cff
NoroiMusha37 <vo.melnyk37@gmail.com>
2026-02-11 21:20:46 +0200

    [add] crawler running functions

---
+++ src/crawler.py
@@ -0,0 +1,53 @@
+import logging
+from datetime import datetime
+from pathlib import Path
+
+from scrapy.crawler import CrawlerProcess
+
+from src.config import MainConfig
+from src.scrape.spiders.python_vacancies_spider import PythonVacanciesSpider
+
+SPIDERS = {
+    "python": PythonVacanciesSpider,
+}
+
+logger = logging.getLogger(__name__)
+
+
+def get_output_path(field: str) -> Path:
+    MainConfig.DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = MainConfig.CSV_FILENAME_FORMAT.format(field=field, timestamp=timestamp)
+
+    return MainConfig.DATA_DIR / filename
+
+
+def get_crawler_settings(output_path: Path) -> dict:
+    settings = MainConfig.CRAWLER_SETTINGS.copy()
+    settings["FEEDS"] = {
+        str(output_path): {
+            "format": "csv",
+            "encoding": "utf-8",
+            "overwrite": True,
+        }
+    }
+
+    return settings
+
+
+def run_spider(spider_key: str):
+    spider_class = SPIDERS.get(spider_key)
+    if not spider_class:
+        logger.error(f"Spider {spider_key} is not supported")
+        raise ValueError(f"Spider {spider_key} not supported")
+
+    output_path = get_output_path(spider_key)
+
+    settings = get_crawler_settings(output_path)
+
+    logger.info(f"Starting spider for {spider_key} vacancies. Saving to {output_path}")
+
+    process = CrawlerProcess(settings=settings)
+    process.crawl(spider_class)
+    process.start()
